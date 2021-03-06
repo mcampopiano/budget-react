dc9982e269fd59e6b7e34bd2c401f3bc3a940e21
Mario Campopiano <mcampopiano92@gmail.com>
2021-03-06 12:49:45 -0600

    wireframed income section of homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import {NavBar} from "./Components/NavBar"
+import {ApplicationViews} from "./Components/ApplicationViews"
 
 function App() {
   return (
+    <>
     <NavBar />
+    <ApplicationViews />
+    </>
   );
 }
 
