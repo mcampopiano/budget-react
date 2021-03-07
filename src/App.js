0cb49ece9530f4e0098ef254daeb24779f9b9354
Mario Campopiano <mcampopiano92@gmail.com>
2021-03-07 17:33:44 -0600

    Implemented and tested Login/register

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-import {NavBar} from "./Components/NavBar"
-import {ApplicationViews} from "./Components/ApplicationViews"
+import { NavBar } from "./Components/NavBar"
+import { ApplicationViews } from "./Components/ApplicationViews"
+import { Login } from './Components/auth/Login';
+import { Register } from './Components/auth/Register';
+import { Redirect, Route } from 'react-router';
 
 function App() {
   return (
     <>
-    <NavBar />
-    <ApplicationViews />
+      <Route render={() => {
+        if (localStorage.getItem("budget_user_id")) {
+          return <>
+            <NavBar />
+            <ApplicationViews />
+          </>
+        } else {
+          return <Redirect to="/login" />
+        }
+      }} />
+
+      <Route path="/login" render={() => {
+        if (localStorage.getItem("budget_user_id")) {
+          return <Redirect to="/" />
+        } else {
+          return <Login />
+        }
+      }} />
+      <Route path="/register" render={() => {
+        if (localStorage.getItem("budget_user_id")) {
+          return <Redirect to="/" />
+        } else {
+          return <Register />
+        }
+      }} />
     </>
   );
 }
