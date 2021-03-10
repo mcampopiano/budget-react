36c47d4355aa06755d3139fa8f8549207b492669
Mario Campopiano <mcampopiano92@gmail.com>
2021-03-09 23:07:25 -0600

    created budgetprovider component and a create function

---
+++ src/Components/budgets/BudgetProvider.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react"
+
+export const BudgetContext = React.createContext()
+
+export const BudgetProvider = props => {
+    const [budgets, setBudgets] = useState([])
+
+    export const createBudget = budget => {
+        return fetch("http://localhost:8000/budgets", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Token ${localStorage.getItem("budget_user_id")}`
+            },
+            body: JSON.stringify(newPost)
+        })
+    }
+
+    return (
+        <BudgetContext.Provider value={{budgets, setBudgets, createBudget}}>
+            {props.children}
+        </BudgetContext.Provider>
+    )
+}
