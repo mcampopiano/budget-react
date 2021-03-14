606492f95caecb3b5e8bc378fa83fd935e4af308
Mario Campopiano <mcampopiano92@gmail.com>
2021-03-14 14:12:24 -0500

    Imported the createDeposit function to the deposit form and successfully tested functionality

--- src/Components/deposits/DepositForm.js
+++ src/Components/deposits/DepositForm.js
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { useHistory } from 'react-router';
 import { Button, Form, FormGroup, Label, Input, ButtonGroup } from 'reactstrap';
+import { DepositContext } from './DepositProvider';
 
 export const DepositForm = (props) => {
     const history = useHistory()
     const budgetId = props.location.state.budgetId
+    const {createDeposit} = useContext(DepositContext)
+    const [deposit, setDeposit] = useState({"source": "", "amount": 0, "date": ""})
+
+    const handleControlledInputChange = e => {
+        const newDeposit = Object.assign({}, deposit)
+        newDeposit[e.target.name] = e.target.value
+        setDeposit(newDeposit)
+    }
+
+    const constructDeposit = () => {
+        createDeposit({
+            budgetId: parseInt(budgetId),
+            source: deposit.source,
+            amount: deposit.amount,
+            date: deposit.date
+        })
+        .then(() => history.push("/"))
+    }
+
     return (
         <Form>
             <FormGroup>
                 <Label for="depositSource">Source</Label>
-                <Input type="text" name="Source" id="depositSource" placeholder="e.g. Paycheck" />
+                <Input type="text" name="source" id="depositSource" placeholder="e.g. Paycheck" onChange={handleControlledInputChange}/>
             </FormGroup>
             <FormGroup>
                 <Label for="exampleDepositAmt">Amount</Label>
-                <Input type="number" name="amount" id="exampleDepositAmt" placeholder="e.g. 1000.00" />
+                <Input type="number" name="amount" id="exampleDepositAmt" placeholder="e.g. 1000.00" onChange={handleControlledInputChange}/>
             </FormGroup>
             <FormGroup>
                 <Label for="depositDate">Date of deposit</Label>
-                <Input type="date" name="Date" id="depositDate" />
+                <Input type="date" name="date" id="depositDate" onChange={handleControlledInputChange}/>
             </FormGroup>
 
             <ButtonGroup>
-                <Button color="success">Submit</Button>
+                <Button color="success"
+                onClick={constructDeposit}>Submit</Button>
                 <Button color="danger" onClick={() => history.goBack()}>Cancel</Button>
             </ButtonGroup>
         </Form>
