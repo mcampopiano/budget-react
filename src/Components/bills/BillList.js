801288b192843ba3aeef2b6c26cf9dff87d2f728
Mario Campopiano <mcampopiano92@gmail.com>
2021-03-23 11:02:00 -0500

    created cards to display budget, actual, and remaining balances on recurring bills page. Currently hard coded data.

--- src/Components/bills/BillList.js
+++ src/Components/bills/BillList.js
@@ -17,30 +17,52 @@ export const BillList = (props) => {
 
     return (
         <>
-            <Table hover>
-                <thead>
-                    <tr>
-                        <th>Biller</th>
-                        <th>Expected amount</th>
-                        <th>Due date</th>
-                        <th>Actual Amount</th>
-                        <th>Date paid</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        bills.map(bill => (
-                            <tr key={bill.id}>
-                                <td>{bill.biller}</td>
-                                <td>${bill.expected_amount}</td>
-                                <td>{bill.due_date}</td>
-                                <td>${bill.payments[0].amount}</td>
-                                <td>{formatDate(bill.payments[0].date_paid)}</td>
-                            </tr>
-                        ))
-                    }
-                </tbody>
-            </Table>
+            <section className="table">
+                <Table hover>
+                    <thead>
+                        <tr>
+                            <th>Biller</th>
+                            <th>Expected amount</th>
+                            <th>Due date</th>
+                            <th>Actual Amount</th>
+                            <th>Date paid</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            bills.map(bill => (
+                                <tr key={bill.id}>
+                                    <td>{bill.biller}</td>
+                                    <td>${bill.expected_amount}</td>
+                                    <td>{bill.due_date}</td>
+                                    <td>${bill.payments[0].amount}</td>
+                                    <td>{formatDate(bill.payments[0].date_paid)}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </Table>
+            </section>
+            <section className="totals">
+                <Card className="totals--card">
+                    <CardTitle tag="h5">Budget</CardTitle>
+                    <CardBody>
+                        <CardText>$500</CardText>
+                    </CardBody>
+                </Card>
+                <Card className="totals--card">
+                    <CardTitle tag="h5">Actual</CardTitle>
+                    <CardBody>
+                        <CardText>$300</CardText>
+                    </CardBody>
+                </Card>
+                <Card className="totals--card">
+                    <CardTitle tag="h5">Remaining</CardTitle>
+                    <CardBody>
+                        <CardText>$200</CardText>
+                    </CardBody>
+                </Card>
+            </section>
         </>
     )
 }
