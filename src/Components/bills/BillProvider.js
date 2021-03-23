fb4a61fb74b814f8060048ccee5de9dea4b5a8bd
Mario Campopiano <mcampopiano92@gmail.com>
2021-03-23 16:56:46 -0500

    created a function for adding new payments

--- src/Components/bills/BillProvider.js
+++ src/Components/bills/BillProvider.js
@@ -26,8 +26,20 @@ export const BillProvider = props => {
         })
     }
 
+    const addPayment = payment => {
+        return fetch(`http://localhost:8000/recurring/${payment.billerId}/payments`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Token ${localStorage.getItem("budget_user_id")}`
+            },
+            body: JSON.stringify(biller)
+        })
+    }
+
+
     return (
-        <BillContext.Provider value={{bills, setBills, getBills, addBiller}}>
+        <BillContext.Provider value={{bills, setBills, getBills, addBiller, addPayment}}>
             {props.children}
         </BillContext.Provider>
     )
