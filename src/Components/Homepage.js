dafd3276be35f9f3bde77a78e09ceece06bf32fd
Mario Campopiano <mcampopiano92@gmail.com>
2021-03-24 09:10:34 -0500

    get the amount spent of total amount to render on the homepage

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -2,16 +2,18 @@ import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import {
     Table, Card, CardText, CardBody,
-    CardTitle, Button,
+    CardTitle, Button, Progress
 } from 'reactstrap';
 import { BudgetContext } from './budgets/BudgetProvider';
 import { DepositContext } from './deposits/DepositProvider';
 import { formatDate } from "./DateFormatter"
 import "./Homepage.css"
+import { EnvelopeContext } from './envelopes/EnvelopeProvider';
 
 export const Homepage = (props) => {
     const { getBudgetById } = useContext(BudgetContext)
     const { deleteDeposit, deposits } = useContext(DepositContext)
+    const { getEnvelopes, envelopes } = useContext(EnvelopeContext)
     const history = useHistory()
     const budgetId = localStorage.getItem("budgetId")
     const [currentBudget, setBudget] = useState({})
@@ -22,12 +24,16 @@ export const Homepage = (props) => {
             .then(setBudget)
     }, [deposits])
 
+    useEffect(() => {
+        getEnvelopes()
+    }, [])
+
 
     return (
         <>
             <div className="homepage">
                 <section className="income">
-                        <h1 className="header">Income</h1>
+                    <h1 className="header">Income</h1>
                     <section className="entries">
                         <Table hover className="table--homepage">
                             <thead>
@@ -57,51 +63,64 @@ export const Homepage = (props) => {
                             </tbody>
                             <Button color="success" onClick={() => history.push("/deposits/form", { budgetId: budgetId })}>Add deposit</Button>
                         </Table>
-                    <article className="totals">
-                        <Card className="totals--card">
-                            <CardTitle tag="h5">Estimated in</CardTitle>
-                            <CardBody>
-                                <CardText>${currentBudget.est_income}</CardText>
-                            </CardBody>
-                        </Card>
-                        <Card className="totals--card">
-                            <CardTitle tag="h5">Actual in</CardTitle>
-                            <CardBody>
-                                <CardText>${currentBudget.actual_inc}</CardText>
-                            </CardBody>
-                        </Card>
-                    </article>
+                        <article className="totals">
+                            <Card className="totals--card">
+                                <CardTitle tag="h5">Estimated in</CardTitle>
+                                <CardBody>
+                                    <CardText>${currentBudget.est_income}</CardText>
+                                </CardBody>
+                            </Card>
+                            <Card className="totals--card">
+                                <CardTitle tag="h5">Actual in</CardTitle>
+                                <CardBody>
+                                    <CardText>${currentBudget.actual_inc}</CardText>
+                                </CardBody>
+                            </Card>
+                        </article>
                     </section>
                 </section>
                 <section className="budget">
                     <h2 className="header">Budget</h2>
                     <section className="budget--items">
-                    <Card className="budget--item">
-                        <CardTitle tag="h5">Total Budget</CardTitle>
-                        <CardBody>
-                            <CardText>${currentBudget.total_budget}</CardText>
-                        </CardBody>
-                    </Card>
-                    <Card className="budget--item">
-                        <CardTitle tag="h5">Total spent</CardTitle>
-                        <CardBody>
-                            <CardText>${currentBudget.total_spent}</CardText>
-                        </CardBody>
-                    </Card>
-                    <Card className="budget--item">
-                        <CardTitle tag="h5">Remaing budget</CardTitle>
-                        <CardBody>
-                            <CardText>${currentBudget.remaining_budget}</CardText>
-                        </CardBody>
-                    </Card>
-                    <Card className="budget--item">
-                        <CardTitle tag="h5">Net total</CardTitle>
-                        <CardBody>
-                            <CardText>${currentBudget.net_total}</CardText>
-                        </CardBody>
-                    </Card>
+                        <Card className="budget--item">
+                            <CardTitle tag="h5">Total Budget</CardTitle>
+                            <CardBody>
+                                <CardText>${currentBudget.total_budget}</CardText>
+                            </CardBody>
+                        </Card>
+                        <Card className="budget--item">
+                            <CardTitle tag="h5">Total spent</CardTitle>
+                            <CardBody>
+                                <CardText>${currentBudget.total_spent}</CardText>
+                            </CardBody>
+                        </Card>
+                        <Card className="budget--item">
+                            <CardTitle tag="h5">Remaing budget</CardTitle>
+                            <CardBody>
+                                <CardText>${currentBudget.remaining_budget}</CardText>
+                            </CardBody>
+                        </Card>
+                        <Card className="budget--item">
+                            <CardTitle tag="h5">Net total</CardTitle>
+                            <CardBody>
+                                <CardText>${currentBudget.net_total}</CardText>
+                            </CardBody>
+                        </Card>
                     </section>
                 </section>
+            </div>
+            <div className="progressBars">
+                {
+                    envelopes.map(envelope => {
+                        if (envelope.user.key === localStorage.getItem('budget_user_id')) {
+                            let totalSpent = 0
+                            envelope.payment.forEach(payment => totalSpent += payment.amount)
+                            return <>
+                                <div className="text-center">${totalSpent} of ${envelope.budget}</div>
+                            </>
+                        }
+                    })
+                }
             </div>
         </>
     );
